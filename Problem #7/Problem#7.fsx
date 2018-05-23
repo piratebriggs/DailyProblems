53c01d2881a790326d7624376da9f974556bda66
abriggs <antony.briggs@gmail.com>
2018-05-23 16:11:44 +0100

    complete

--- Problem #7/Problem#7.fsx
+++ Problem #7/Problem#7.fsx
@@ -9,14 +9,27 @@ let encryptionKeys = charMap |> Map.toSeq |> Seq.map fst
 let toString (inp:int list) = 
   List.fold (fun s v -> s + (string v)) "" inp
 
-let rec problem goal (results:int list list) current = 
+let rec solve goal (results:int list list) current = 
   let s = toString current
   if s = goal then
     current::results
   elif s.Length < goal.Length then
-    let z = Seq.fold (fun s v -> problem goal s (v::current)) results encryptionKeys
+    let z = Seq.fold (fun s v -> solve goal s (v::current)) results encryptionKeys
     z
   else
     results
 
-problem "111" [] [] 
+
+let decode cipher = 
+  Seq.fold (fun s v -> s + string charMap.[v] ) "" cipher
+
+let solutions goal = 
+  let ciphers = solve goal [] []
+  Seq.fold (fun s v -> (decode v)::s) [] ciphers
+
+let problem goal = 
+  List.length (solve goal [] [])
+
+solutions "111"
+
+problem "111"
