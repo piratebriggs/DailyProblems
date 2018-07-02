40ef14f3dcafc7bd9b0a58cedda3ef2de8874531
abriggs <antony.briggs@gmail.com>
2018-07-02 16:21:59 +0100

    fixed now that case 3 is correct!

--- Kata/findEvenIndex.fsx
+++ Kata/findEvenIndex.fsx
@@ -1,9 +1,8 @@
-type Zipper<'a> = Zipper of ('a list * 'a * 'a list) with
+type Zipper = Zipper of (int list * int * int list) with
     static member create l =
         match l with
         | [] -> failwith "oops"
-        | [z] -> Zipper ([], 0, z::[])
-        | h::l -> Zipper ([], 0, h::l)
+        | h :: t -> Zipper ([], h, t)
 
     member z.right () =
         match z with
@@ -12,15 +11,15 @@ type Zipper<'a> = Zipper of ('a list * 'a * 'a list) with
 
 let isEvenIndex zipper = 
     match zipper with
-    | Zipper([], z, right) ->  printf "1";0 = List.sum right
-    | Zipper(left, z, []) ->  printf "2";List.sum left = 0
-    | Zipper(left, _, right) ->  printf "3";List.sum left = List.sum right
+    | Zipper([], z, right) ->  0 = List.sum right
+    | Zipper(left, z, []) ->  List.sum left = 0
+    | Zipper(left, _, right) ->  List.sum left = List.sum right
 
 let rec FindIndex zipper func = 
     if func zipper then 
         match zipper with
         | Zipper([], _, _) -> 0
-        | Zipper(l, _, _) -> List.length l-1
+        | Zipper(l, _, _) -> List.length l
     else 
         match zipper with
         | Zipper(_, _, []) -> -1
@@ -29,12 +28,15 @@ let rec FindIndex zipper func =
 let findEvenIndex (items : int array) = 
     match items with
     | [||] -> 0
+    | [|_|] -> 0
     | _ -> FindIndex (items |> Array.toList |> Zipper.create) isEvenIndex
 
 
 findEvenIndex [|1; 2; 3; 4; 3; 2; 1|]   // 3
 findEvenIndex [|1; 100; 50; -51; 1; 1|] // 1
-findEvenIndex [|10;-80;10;10;15;35|]    // 0
+findEvenIndex [|20;10;-80;10;10;15;35|]    // 0
+findEvenIndex [||]    // 0
 findEvenIndex [|10|]    // 0
-findEvenIndex [|-46; 15; 14; 17; 0|]    // 4 - error
+findEvenIndex [|-46; 15; 14; 17; 0|]    // 4 
+
 
