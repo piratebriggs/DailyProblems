9f6b532f5a409e916d70e3e62503f3f3883e4ff6
abriggs <antony.briggs@gmail.com>
2018-07-04 08:44:16 +0100

    second kata

--- Kata/Mul3and5.fsx
+++ Kata/Mul3and5.fsx
@@ -1,14 +1,7 @@
+let folder n =
+    n % 3 = 0 || n % 5 = 0
+
 let solve n = 
-    let threeorfive inp = 
-        inp % 3 = 0 || inp % 5 = 0
+    [1..n-1] |> List.filter folder |> List.sum
 
-    let rec threeandfive (state:int list) inp =
-        if inp = 0 then 
-            state
-        else
-            let newstate = if (threeorfive inp) then (inp::state) else state
-            threeandfive newstate (inp-1)
-
-    List.sum (threeandfive [] (n-1))
-
-solve 10
+solve 10    // 23
